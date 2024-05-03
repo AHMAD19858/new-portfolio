dbf3fd471439e793b138c7fe57655b5e1fee2e37
Ahmed Essam <aesam@devsolutions.team>
2024-05-03 11:38:13 +0300

    d

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -70,7 +70,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+          <div className=' p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
                 <Image src={Firebase} width='64px' height='64px' alt='/' />
