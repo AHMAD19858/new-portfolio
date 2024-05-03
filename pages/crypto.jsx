ff1679da9188f1efd7714d577ec56dcd92eccdfd
Ahmed Essam <aesam@devsolutions.team>
2024-05-03 11:45:11 +0300

    X

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import cryptoImg from '../public/assets/projects/crypto.jpg';
-import dddd from '../public/assets/projects/capture.PNG'
+/* import dddd from '../public/assets/projects/capture.PNG' */
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
@@ -14,7 +14,7 @@ const crypto = () => {
           className='absolute z-1'
           layout='fill'
           objectFit='cover'
-          src={dddd}
+          src={cryptoImg}
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
